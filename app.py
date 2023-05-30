1b534a2776209ca0a0ef65dae084c1aa5afc74bc
Chintya06 <chintyadp6@gmail.com>
2023-05-30 15:19:51 +0700

    freeze dotenv and other requirements

--- app.py
+++ app.py
@@ -1,5 +1,16 @@
+import os
+from os.path import join, dirname
+from dotenv import load_dotenv
+
 from flask import Flask, render_template, request, jsonify
 from pymongo import MongoClient
+
+dotenv_path = join(dirname(__file__), '.env')
+load_dotenv(dotenv_path)
+
+MONGODB_URI = os.environ.get("mongodb+srv://chintya:sparta@cluster0.vywmvbi.mongodb.net/?retryWrites=true&w=majority")
+DB_NAME = os.environ.get("dbsparta")
+
 client = MongoClient('mongodb+srv://chintya:sparta@cluster0.vywmvbi.mongodb.net/?retryWrites=true&w=majority')
 db = client.dbsparta
 app = Flask(__name__)
@@ -10,13 +21,20 @@ def home():
 
 @app.route("/homework", methods=["POST"])
 def homework_post():
-    sample_receive = request.form['sample_give']
-    print(sample_receive)
+    # sample_receive = request.form['sample_give']
+    name_receive = request.form['name_give']
+    comment_receive = request.form['comment_give']
+    doc = {
+        'name': name_receive,
+        'comment': comment_receive,
+    }
+    db.fanmessages.insert_one(doc)
     return jsonify({'msg':'POST request!'})
 
 @app.route("/homework", methods=["GET"])
 def homework_get():
-    return jsonify({'msg':'GET request!'})
+    message_list = list(db.fanmessages.find({}, {'_id': False}))
+    return jsonify({'messages': message_list})
 
 if __name__ == '__main__':
     app.run('0.0.0.0', port=5000, debug=True)
